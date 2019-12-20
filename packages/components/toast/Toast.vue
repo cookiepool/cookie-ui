22646007dc1e111ef6f538776795350e7c40055d
cookiepool <18381317534@163.com>
2019-12-20 17:11:15 +0800

    Add toast component

---
+++ packages/components/toast/Toast.vue
@@ -0,0 +1,22 @@
+<template>
+  <div v-if="show" class="cookie-toast-mask">
+    <div class="cookie-toast-dialog">
+      <p class="toast-content">{{ message }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ck-toast',
+  data() {
+    return {
+      show: false,
+      message: ''
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
