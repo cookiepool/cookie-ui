e51865f2fa4196bd4b9120334b712fba3bce27d6
cookiepool <18381317534@163.com>
2019-12-24 13:56:51 +0800

    modify

--- examples/main.js
+++ examples/main.js
@@ -2,8 +2,12 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router/router.js';
 
+import Header from './components/header/HeadCommon.vue';
+Vue.component('app-header', Header);
+
 // 引入组件（注册所有）
-import CookieUI from '../packages/index';
+import CookieUI from '../lib/index.js';
+import '../lib/index.css';
 Vue.use(CookieUI);
  
 new Vue({
